168d6d377145e92850b5fd813c20fbae61a53691
Adrian Pietrzak <35579815+pietrzakadrian@users.noreply.github.com>
2018-05-28 19:30:32 +0200

    Update all.js

--- js/all.js
+++ js/all.js
@@ -6,48 +6,17 @@ $("document").ready (
         $("button").click(
             function()
             {
-                if ($("input#checkbox_yoda").is(':checked'))
-                {
+                if ($("input#checkbox_yoda").is(':checked') || $("input#checkbox_yoda").is(':checked') && $("input#checkbox_vader").is(':checked') || $("input#checkbox_yoda").is(':checked') && $("input#checkbox_trooper").is(':checked') || $("input#checkbox_yoda").is(':checked') && $("input#checkbox_vader").is(':checked') && $("input#checkbox_trooper").is(':checked'))
                     return true;
-                }
 
-                else if ($("input#checkbox_trooper").is(':checked'))
+                else if ($("input#checkbox_trooper").is(':checked') || $("input#checkbox_vader").is(':checked') || $("input#checkbox_trooper").is(':checked') && $("input#checkbox_vader").is(':checked'))
                 {
                     audio.play();
                     return false;
-                }
-
-                else if ($("input#checkbox_vader").is(':checked'))
-                {
-                    audio.play();
-                    return false;
-                }
-
-                else if ($("input#checkbox_yoda").is(':checked') && $("input#checkbox_trooper").is(':checked'))
-                {
-                    return true;
-                }
-
-                else if ($("input#checkbox_trooper").is(':checked') && $("input#checkbox_vader").is(':checked'))
-                {
-                    audio.play();
-                    return false;
-                }
-
-                else if ($("input#checkbox_yoda").is(':checked') && $("input#checkbox_vader").is(':checked'))
-                {
-                    return true;
-                }
-
-                else if ($("input#checkbox_yoda").is(':checked') && $("input#checkbox_vader").is(':checked') && $("input#checkbox_trooper").is(':checked'))
-                {
-                    return true;
                 }
 
                 else
-                {
                     return true;
-                }
             }
         )
     }
